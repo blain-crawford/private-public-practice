b03ef52af67a86432342482c2b0b2bed073cc28c
Blain Crawford <blaincrawford@gmail.com>
2022-01-25 11:23:51 -0500

    fumbling around with pricate scope

--- scripts.js
+++ scripts.js
@@ -1,4 +1,21 @@
 const h1 = document.createElement('h1');
-let body = document.querySelector('body')
-h1.textContent = 'Hello, World';
+h1.textContent = '0';
+const body = document.querySelector('body');
+const counterButton = document.createElement('button');
+counterButton.textContent = 'CLICK TO INCREASE COUNT';
+
+const increaseCount = () => {
+  let counter = 0;
+  return () => {
+    h1.textContent = '';
+    h1.textContent = counter;
+    counter++;
+  }
+};
+let counter = increaseCount()
+
+
+
 body.appendChild(h1);
+body.appendChild(counterButton);
+counterButton.addEventListener('click', counter, false);
