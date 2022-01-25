64944f5c6646f898327db7f85df8b30dc16351d8
Blain Crawford <blaincrawford@gmail.com>
2022-01-25 15:25:29 -0500

    both basic objects functions with buttons

--- scripts.js
+++ scripts.js
@@ -1,8 +1,51 @@
+const h1Math = document.createElement('h1');
+h1Math.textContent = '0';
+const body = document.querySelector('body');
+const increaseButton = document.createElement('button');
+increaseButton.textContent = 'Increase!';
+const decreaseButton = document.createElement('button');
+decreaseButton.textContent = 'Decrease!';
+body.appendChild(h1Math);
+body.appendChild(increaseButton);
+body.appendChild(decreaseButton);
+
+const alterCounter = {
+  counter: 0,
+  increaseOrDecrease: function() {
+    if(this.textContent === 'Increase!') {
+      alterCounter.counter++;
+      h1Math.textContent = '';
+      h1Math.textContent = alterCounter.counter;
+    } else if (this.textContent === 'Decrease!') {
+      alterCounter.counter--;
+      h1Math.textContent = '';
+      h1Math.textContent = alterCounter.counter; 
+    }
+  }
+};
+
+increaseButton.addEventListener('click', alterCounter.increaseOrDecrease.bind(increaseButton), false);
+decreaseButton.addEventListener('click', alterCounter.increaseOrDecrease.bind(decreaseButton), false)
+
+
+
+
+
+
+
+
+
+
+
+
+
 const h1 = document.createElement('h1');
 h1.textContent = '0';
-const body = document.querySelector('body');
 const nameButton = document.createElement('button');
+const neverButton = document.createElement('button');
 nameButton.textContent = 'SAY MY NAME!';
+neverButton.textContent = 'NO!';
+
 const nameSayer = {
   name: 'Heisenberg',
   no: 'NO!',
@@ -14,12 +57,14 @@ const nameSayer = {
     }
     if(this.textContent === 'NO!') {
       h1.textContent = '';
-      h1.textContent = this.no
-      console.log(this);
+      h1.textContent = nameSayer.no
+      console.log(this.textContent);
     }
   }
 };
 
 body.appendChild(h1);
 body.appendChild(nameButton);
+body.appendChild(neverButton);
 nameButton.addEventListener('click', nameSayer.sayIt.bind(nameButton), false);
+neverButton.addEventListener('click', nameSayer.sayIt.bind(neverButton), false);
